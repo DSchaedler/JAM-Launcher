ceefb0a8e918ea8251470256dd229a2359f89428
Dee <dominicschaedler@gmail.com>
2019-07-20 12:06:58 -0400

    Remove unnecessary files

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { remote } = require('electron');
-const { ipcRenderer } = remote;
-
-let currWindow = remote.BrowserWindow.getFocusedWindow();
-
-window.closeCurrentWindow = function(){
-  currWindow.close();
-}
